29a21e1b5a74d70cadd06090936edf7a32544304
kane lariviere <91632194+knlrvr@users.noreply.github.com>
2023-02-23 16:46:46 -0500

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,8 +17,9 @@ export default function Home() {
     <div className="bg-gray-200 dark:bg-black dark:text-white h-screen overflow-x-scroll">
       <Head>
         <title>Kane Lariviere</title>
-        {/** meta */}
-
+        <meta name="description" content="Projects | Kane Lariviere" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <NewCursor />
