d31c96bee999a49e453232a40e5b11e28787ec67
kane lariviere <91632194+knlrvr@users.noreply.github.com>
2023-03-03 15:27:01 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
+import Link from 'next/link'
 
-import {
-  BsMoon,
-  BsSun
-} from 'react-icons/bs'
+import { FaBomb } from 'react-icons/fa'
 
 import useDarkMode from './Theme'
 
@@ -12,8 +10,15 @@ type Props = {}
 export default function Footer({}: Props) {
   const [colorTheme, setTheme] = useDarkMode();
   return (
+    <>
     <div className="fixed bottom-6 text-xs text-center inset-x-0 w-full text-black dark:text-white tracking-[8px] z-50">
         &copy; / 2023
     </div>
+    <div className="fixed bottom-6 left-6 text-xs w-full text-black dark:text-white tracking-widest z-50">
+        <Link href="/Minesweeper">
+          <FaBomb />
+        </Link>
+    </div>
+    </>
   )
 }
